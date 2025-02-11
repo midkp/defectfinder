68994f4468f5243be34633b0b5d551d73300d56b
midkp <midhunkrishnan.ai@gmail.com>
2025-02-11 18:38:13 +0530

    Removed API Key from utils.py

--- app/config.py
+++ app/config.py
@@ -8,7 +8,7 @@ load_dotenv()
 
 class Settings:
     def __init__(self):
-        self.OPENAI_API_KEY = os.getenv('OPENAI_API_KEY', 'ysk-proj-Lzu0jlXDzdXbqMhRP6ywsz_WXSYqVe117YLuLyHLZZ9ZWjjooa5PFfJV_DcJfxvijSnDTR59rtT3BlbkFJmxKI9Gfh5sLL9Q8yCMgIRIXR3qg_5ysreaZ_olyjCtwdwPrAP_zRUip3RVSOIyjx8OkgpqFhwA')
+        self.OPENAI_API_KEY = os.getenv('OPENAI_API_KEY', 'api-key-value')
         self.OPENAI_MODEL = os.getenv('OPENAI_MODEL', 'gpt-4o-mini')  # Add this line
 
 settings = Settings()
